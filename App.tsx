7b3c57eb50a060773b8542262cb57d7bc1947abd
Denys Vynohradnyi <denv0808@gmail.com>
2022-12-27 05:31:35 +0100

    Created CustomButton, added plus button (logic and style), added redux

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 
 import React from 'react';
-import MainView from './src/ui/screen/MainView';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
+import MainView from './src/ui/screen/Main/MainView';
 
 const App = (): JSX.Element => {
 
   return <>
-      <MainView />
+  <Provider store={store}>
+    <View style={{flex:3}}/>
+      <View style={{flex:3}}>
+        <MainView />
+      </View>
+    <View style={{flex:3}}/>
+  </Provider>
   </>
 }
 
--- src/ui/screen/Main/MainView.tsx
+++ src/ui/screen/Main/MainView.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { selectCount, increment } from '../../../redux/counterSlice';
+import { useAppDispatch, useAppSelector } from '../../../redux/redux-hooks';
 import MainContent from './MainContent';
 
 interface Props {}
 
 const MainView = (props: Props): JSX.Element => {
 
-    return <MainContent value={0}/>
+    let value = useAppSelector(selectCount);
+    let dispatch = useAppDispatch();
+
+    let incrementCounter = (amount: number) => {
+        dispatch(increment(amount));
+    }
+
+    return <MainContent value={value} increment={incrementCounter}  />
 }
 
 export default MainView;
