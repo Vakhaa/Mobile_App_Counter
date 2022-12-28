cdad8084eb80c716df72b1da0bb7a44e2d13fba7
Denys Vynohradnyi <denv0808@gmail.com>
2022-12-28 03:51:19 +0100

    Added and change the logic of counterSlice. Change Buttons for the Automatically (holding), created tooltip, and hook usePeriodically

--- src/redux/counterSlice.tsx
+++ src/redux/counterSlice.tsx
@@ -1,33 +1,41 @@
 import {createSlice, PayloadAction } from '@reduxjs/toolkit'
 import {RootState} from './store';
 
-// Define a type for the slice state
+
+//Define a type for the slice state
 interface CounterState {
     value: number,
-    buttonLongPressValue: number
+    longPressValue: number,
   }
   
-  // Define the initial state using that type
+  //Define the initial state using that type
   const initialState: CounterState = {
     value: 0,
-    buttonLongPressValue: 0
+    longPressValue: 0,
   }
 
 const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
-    changeCounterValue: (state, action: PayloadAction<number>) => {
-      if((state.value + action.payload) <= 100 && (state.value + action.payload) >= 0)
+    changeCounterValueAction: (state, action: PayloadAction<number>) => {
+      if((state.value + action.payload) <= 100 && (state.value + action.payload) >= 0){
         state.value += action.payload;
-    }
+      }      
+    },
+    changeLongPressValueAction: (state, action: PayloadAction<number>) => {
+      state.longPressValue += action.payload;
+    },
+    unpressButtonAction: (state) => { //reset the temp data
+      state.longPressValue = 0;
+    },
   }
 })
 
-export const { changeCounterValue } = counterSlice.actions
+export const { changeCounterValueAction, changeLongPressValueAction, unpressButtonAction } = counterSlice.actions
 
 // Other code such as selectors can use the imported `RootState` type
 export const selectCount = (state: RootState) => state.counter.value;
-export const selectButtonLongPressValue = (state: RootState) => state.counter.buttonLongPressValue;
+export const selectButtonLongPressValue = (state: RootState) => state.counter.longPressValue;
 
 export default counterSlice.reducer;
--- src/ui/screen/Main/MainContent.tsx
+++ src/ui/screen/Main/MainContent.tsx
@@ -1,51 +1,55 @@
 import React from 'react'
-import { StyleSheet,  View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Spacing } from '../../../styles';
 import Counter from '../../components/Counter';
-import CustomButton from '../../components/CustomButton';
+import MinusButton from '../../components/MinusButton';
+import PlusButton from '../../components/PlusButton';
 
 interface Props {
   value: number,
-  changeValue: (amount:number) => void,
+  longPressValue: number,
+  onPressOut: () => void,
+  changeValue: (amount: number) => void,
+  changeLongPressValue: (amount: number) => void,
 }
+
 
 const MainContent = (props: Props): JSX.Element => {
 
-    return <>
+  return <>
     <View style={styles.container}>
       {/* Counter */}
       <View>
-        <Counter value={props.value} text={"Counter:"}/>
+        <Counter value={props.value} text={"Counter:"} />
       </View>
       {/* Buttons */}
       <View style={styles.buttons}>
         <View style={styles.button}>
-          <CustomButton title={"+"} onPress={()=>{props.changeValue(+1)}} isActive={props.value >=100}/>
+          <PlusButton {...props} />
         </View>
-        <View style={[styles.button, {marginLeft: Spacing.m}]}>
-          <CustomButton title={"-"} onPress={()=>{props.changeValue(-1)}} isActive={props.value <=0}/>
+        <View style={[styles.button, { marginLeft: Spacing.m }]}>
+          <MinusButton {...props} />
         </View>
       </View>
     </View>
-    </>
+  </>
 }
 
 export default MainContent;
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
     justifyContent: 'center',
-    alignItems:'center',
+    alignItems: 'center',
   },
-  buttons:{
-    width:"100%",
+  buttons: {
+    width: "100%",
     marginTop: Spacing.m,
-    flexDirection:'row',
+    flexDirection: 'row',
     justifyContent: 'center'
   },
-  button:{
+  button: {
     width: "33%"
   }
 });
-  
